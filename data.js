e8b0c45f0b668f1b7d7be1db47726ffd45dd2ab4
adrianovie-rgb <adriano_vie@hotmail.com>
2026-05-22 19:25:56 -0300

    Atualização automática Fri May 22 2026 00:00:00 GMT-0300 (Brasilia Standard Time) Sat Dec 30 1899 19:25:00 GMT-0306 (Brasilia Standard Time)

---
+++ data.js
@@ -0,0 +1,925 @@
+var D = {
+  "lastUpdate": "Fri May 22 2026 00:00:00 GMT-0300 (Brasilia Standard Time) Sat Dec 30 1899 19:25:00 GMT-0306 (Brasilia Standard Time)",
+  "meta": {
+    "ppp": 175,
+    "recv": 98.5,
+    "stow": 99.5,
+    "dep": 99.5
+  },
+  "regional": "DF-SUL",
+  "weeks": [
+    "W15",
+    "W16",
+    "W17",
+    "W18",
+    "W19",
+    "W20"
+  ],
+  "semana_ref": "W20",
+  "feriados": {
+    "W15": "Sexta-feira Santa (11/04)",
+    "W17": "Tiradentes (21/04)",
+    "W18": "Dia do Trabalhador (01/05)"
+  },
+  "node_order": [
+    "ECB8",
+    "EGO8",
+    "ERS8",
+    "ESB8",
+    "ESE8",
+    "EIJ8",
+    "EJO8",
+    "ELO8",
+    "EME8",
+    "EMR8",
+    "EOZ8",
+    "EUM8",
+    "EVE8"
+  ],
+  "nodes": {
+    "ECB8": {
+      "grupo": "",
+      "tipo": "",
+      "d1_stow": false,
+      "d1_dep": false,
+      "stow_d1_origens": "",
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog_acc": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EGO8": {
+      "grupo": "",
+      "tipo": "",
+      "d1_stow": false,
+      "d1_dep": false,
+      "stow_d1_origens": "",
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog_acc": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "ERS8": {
+      "grupo": "",
+      "tipo": "",
+      "d1_stow": false,
+      "d1_dep": false,
+      "stow_d1_origens": "",
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog_acc": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "ESB8": {
+      "grupo": "",
+      "tipo": "",
+      "d1_stow": false,
+      "d1_dep": false,
+      "stow_d1_origens": "",
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog_acc": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "ESE8": {
+      "grupo": "",
+      "tipo": "",
+      "d1_stow": false,
+      "d1_dep": false,
+      "stow_d1_origens": "",
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog_acc": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EIJ8": {
+      "grupo": "",
+      "tipo": "",
+      "d1_stow": false,
+      "d1_dep": false,
+      "stow_d1_origens": "",
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog_acc": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EJO8": {
+      "grupo": "",
+      "tipo": "",
+      "d1_stow": false,
+      "d1_dep": false,
+      "stow_d1_origens": "",
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog_acc": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "ELO8": {
+      "grupo": "",
+      "tipo": "",
+      "d1_stow": false,
+      "d1_dep": false,
+      "stow_d1_origens": "",
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog_acc": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EME8": {
+      "grupo": "",
+      "tipo": "",
+      "d1_stow": false,
+      "d1_dep": false,
+      "stow_d1_origens": "",
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog_acc": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EMR8": {
+      "grupo": "",
+      "tipo": "",
+      "d1_stow": false,
+      "d1_dep": false,
+      "stow_d1_origens": "",
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog_acc": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EOZ8": {
+      "grupo": "",
+      "tipo": "",
+      "d1_stow": false,
+      "d1_dep": false,
+      "stow_d1_origens": "",
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog_acc": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EUM8": {
+      "grupo": "",
+      "tipo": "",
+      "d1_stow": false,
+      "d1_dep": false,
+      "stow_d1_origens": "",
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog_acc": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "EVE8": {
+      "grupo": "",
+      "tipo": "",
+      "d1_stow": false,
+      "d1_dep": false,
+      "stow_d1_origens": "",
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "backlog_acc": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    }
+  },
+  "edsps": {
+    "EIJ8": {
+      "semanas": [],
+      "recv": [],
+      "stow": [],
+      "dep": [],
+      "ppp": [],
+      "cpt": [],
+      "otd": []
+    },
+    "EJO8": {
+      "semanas": [],
+      "recv": [],
+      "stow": [],
+      "dep": [],
+      "ppp": [],
+      "cpt": [],
+      "otd": []
+    },
+    "ELO8": {
+      "semanas": [],
+      "recv": [],
+      "stow": [],
+      "dep": [],
+      "ppp": [],
+      "cpt": [],
+      "otd": []
+    },
+    "EME8": {
+      "semanas": [],
+      "recv": [],
+      "stow": [],
+      "dep": [],
+      "ppp": [],
+      "cpt": [],
+      "otd": []
+    },
+    "EMR8": {
+      "semanas": [],
+      "recv": [],
+      "stow": [],
+      "dep": [],
+      "ppp": [],
+      "cpt": [],
+      "otd": []
+    },
+    "EUM8": {
+      "semanas": [],
+      "recv": [],
+      "stow": [],
+      "dep": [],
+      "ppp": [],
+      "cpt": [],
+      "otd": []
+    },
+    "EVE8": {
+      "semanas": [],
+      "recv": [],
+      "stow": [],
+      "dep": [],
+      "ppp": [],
+      "cpt": [],
+      "otd": []
+    }
+  }
+};
